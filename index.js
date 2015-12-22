2cb7aa96467cedaa2a35cb23046ebd1fa34a9d25
Patrick Williams <pwmckenna@gmail.com>
2015-12-21 20:58:55 -0800

    factor js and templates into seperate files

---
+++ index.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+  function sequence (items, callback) {
+    return _.reduce(items, function (promise, item) {
+      return promise.then(function () {
+        return callback(item);
+      })
+    }, Q.resolve());
+  };
+  Q.fcall(function () {
+    return $.get('templates/tid.handlebars');
+  }).then(function (resp) {
+    var repo = 'pwmckenna/til';
+
+    var template = Handlebars.compile(resp);
+    marked.setOptions({
+      highlight: function (code) {
+        return hljs.highlightAuto(code).value;
+      }
+    });
+    Q.fcall(function () {
+      return $.ajax('https://api.github.com/repos/' + repo + '/issues');
+    }).then(function (tils) {
+      var sortedTils = _.sortBy(tils, '-created_at');
+      sortedTils.forEach(function (til) {
+        $('#til').append(template({
+          id: 'til-' + til.id,
+          title: til.title,
+          body: marked(til.body),
+          date: moment(til.created_at).format('MMMM Do YYYY'),
+          labels: til.labels,
+          user: til.user,
+          issue: til.html_url
+        }));
+      });
+    });
+  });
+});
