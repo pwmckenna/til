b91161bb43f54183d10d0f75c95ee5f281ab9694
Patrick Williams <pwmckenna@gmail.com>
2015-12-25 09:31:03 -0800

    webpack conversion from cdn

--- app/main.js
+++ app/main.js
@@ -2,4 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './Components/App';
 
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
+import 'font-awesome/css/font-awesome.css';
+
 ReactDOM.render(<App />, document.getElementById('main'));
--- webpack.module.js
+++ webpack.module.js
@@ -11,5 +11,12 @@ module.exports = {
     test: /\.less$/,
     exclude: /node_modules/,
     loader: 'style!css!less'
+  }, {
+    test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+    loader: 'url-loader?limit=10000&minetype=application/font-woff'
+  },
+  {
+    test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+    loader: 'file-loader'
   }]
 };
