6b2549d14fbbbf7d616a023bf77ccbcc48ce87e4
Patrick Williams <pwmckenna@gmail.com>
2015-12-28 20:51:47 -0800

    config

--- app/containers/asyncProps.jsx
+++ app/containers/asyncProps.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 export default (Component, propTypes, fetch) => {
   return React.createClass({
     propTypes,
-    componentDidMount: function () {
+    componentDidMount() {
       fetch(this.props).then(props => this.setState(props));
     },
-    render: function () {
+    render() {
       return <Component {...this.props} {...this.state} />;
     }
   });
