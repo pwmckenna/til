5fc1ca923102a0b6861cce06a77f706f496667a4
Patrick Williams <pwmckenna@gmail.com>
2016-01-01 13:44:48 -0800

    seperate pages

--- app/Components/Issues.jsx
+++ app/Components/Issues.jsx
@@ -1,16 +1,8 @@
 import React, { Component, PropTypes } from 'react';
-import Q from 'q';
-import $ from 'jquery';
-import _ from 'lodash';
 
 import Fade from './Fade';
 import Issue from './Issue';
 import Loader from './Loader';
-
-import asyncProps from '../containers/asyncProps';
-
-import slug from '../utils/slug';
-import config from '../config';
 
 import './Issues.less';
 
@@ -40,18 +32,4 @@ class Issues extends Component {
   }
 }
 
-export default asyncProps(Issues, props => {
-  const issueFilter = issue => (
-    // if we're on a single issue page, filter for just that issue
-    !props.params.til || slug(issue.title) === props.params.til
-  );
-  const labelFilter = issue => (
-    // if we're on a label page, filter for the issues that have that label
-    !props.params.label || issue.labels.find(label => slug(label.name) === props.params.label)
-  );
-
-  return Q.resolve($.ajax(`https://api.github.com/repos/${config.repo}/issues`))
-    .then(issues => _.sortBy(issues, '-created_at'))
-    .then(issues => issues.filter(issueFilter).filter(labelFilter))
-    .then(issues => ({ issues }));
-});
+export default Issues;
--- app/main.js
+++ app/main.js
@@ -3,9 +3,16 @@ import { render } from 'react-dom';
 import { Router, Route } from 'react-router';
 import { createHashHistory } from 'history';
 import $ from 'jquery';
+import Q from 'q';
+import _ from 'lodash';
 
 import Layout from './Components/Layout';
 import Issues from './Components/Issues';
+
+import asyncProps from './containers/asyncProps';
+
+import slug from './utils/slug';
+import config from './config';
 
 // setup google analytics
 import './utils/ga';
@@ -23,6 +30,36 @@ if (localStorage.githubToken) {
   });
 }
 
+const fetchIssues = () => Q.resolve($.ajax(`https://api.github.com/repos/${config.repo}/issues`));
+const sortIssues = issues => _.sortBy(issues, '-created_at');
+
+const LabelIssuesPage = asyncProps(Issues, props => {
+  const labelFilter = issues => issues.filter(issue => (
+    issue.labels.find(label => slug(label.name) === props.params.label)
+  ));
+
+  return fetchIssues()
+    .then(sortIssues)
+    .then(labelFilter)
+    .then(issues => ({ issues }));
+});
+
+const IssuePage = asyncProps(Issues, props => {
+  const issueFilter = issues => issues.filter(issue => (
+    // if we're on a single issue page, filter for just that issue
+    slug(issue.title) === props.params.til
+  ));
+  return fetchIssues()
+    .then(issueFilter)
+    .then(issues => ({ issues }));
+});
+
+const IssuesPage = asyncProps(Issues, () => {
+  return fetchIssues()
+    .then(sortIssues)
+    .then(issues => ({ issues }));
+});
+
 // setup routing
 const history = createHashHistory({
   queryKey: false
@@ -35,17 +72,17 @@ render((
     <Route component={Layout}>
       <Route
         path="/"
-        component={Issues}
+        component={IssuesPage}
         onEnter={onEnter}
       />
       <Route
         path="til/:til"
-        component={Issues}
+        component={IssuePage}
         onEnter={onEnter}
       />
       <Route
         path="label/:label"
-        component={Issues}
+        component={LabelIssuesPage}
         onEnter={onEnter}
       />
     </Route>
