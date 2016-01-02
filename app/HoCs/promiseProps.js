7b5291017e94f617de94e20d98da05f80dbe2f0e
Patrick Williams <pwmckenna@gmail.com>
2016-01-01 18:47:12 -0800

    offload data storage to a store to avoid errant setStates after unmounting

--- app/HoCs/promiseProps.js
+++ app/HoCs/promiseProps.js
@@ -1,12 +1,40 @@
 import React, { Component as ReactComponent } from 'react';
 
+class Store {
+  constructor() {
+    this.state = {};
+    this.listeners = [];
+  }
+  getState() {
+    return this.state;
+  }
+  setState(state) {
+    this.state = state;
+    this.listeners.forEach(listener => listener());
+  }
+  listen(listener) {
+    this.listeners.push(listener);
+  }
+  unlisten() {
+    this.listeners = [];
+  }
+}
+
 export default fetch => Component => (
   class extends ReactComponent {
+    constructor() {
+      super();
+      this.store = new Store();
+      this.store.listen(() => this.setState(this.store.getState()));
+    }
     componentDidMount() {
-      fetch(this.props).then(props => this.setState(props));
+      fetch(this.props).then(props => this.store.setState(props));
     }
     componentWillReceiveProps(nextProps) {
-      fetch(nextProps).then(props => this.setState(props));
+      fetch(nextProps).then(props => this.store.setState(props));
+    }
+    componentWillUnmount() {
+      this.store.unlisten();
     }
     render() {
       return <Component {...this.props} {...this.state} />;
